dbd03e502b87e5d6c11b346f555c2d4a0f0b9164
Joshua Higginbotham <joshua.higginbotham@codenamesql.com>
2020-12-13 17:37:06 -0500

    #9 update to account for working dir issue

--- (6) main_extract_parquet.py
+++ (6) main_extract_parquet.py
@@ -5,17 +5,19 @@ from Functions.file_processes import *
 import pandas as pd 
 import sys 
 import configparser
+import os
 
 # extract_name = sys.argv[1] # Passed in through SQL Agent Job
 extract_name = 'test.parquet'
 
 # Load Config
+config_file = os.path.join(os.path.dirname(__file__), 'Config/config.ini')
 config = configparser.ConfigParser()
-config.read('Config/config.ini')
+config.read(config_file)
 
 # Initialize Variables
 eng_conn = config['Dev']['conn_string']
-extract_path = config['Dev']['extract_path']
+extract_path = os.path.join(os.path.dirname(__file__),config['Dev']['extract_path'])
 
 salesDF = read_sql(factsales_query, eng_conn)
 
